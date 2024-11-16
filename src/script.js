0fd98e0eb893f9fc976c1468f8eceb37c913f9fc
lynnec23 <lynnecarter23@gmail.com>
2024-11-16 20:50:02 +0800

    updated apiKey

--- src/script.js
+++ src/script.js
@@ -11,7 +11,7 @@ function displayTemperature(response) {
     let searchInputElement = document.querySelector("#search-input");
     let city = searchInputElement.value;
   
-    let apiKey = "b2a5adcct04b33178913oc335f405433";
+    let apiKey = "3o875719afaefcd9btd0cb97235341b2";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   
     axios.get(apiUrl).then(displayTemperature);
